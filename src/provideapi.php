2117b52833c5beadb225562d85baed975a3b884e
Alan Guitard <alaguitard@emi.u-bordeaux1.fr>
2016-10-18 11:30:02 +0200

    Setup de l'environnement TODO: Concevoir SQL

---
+++ src/provideapi.php
@@ -0,0 +1,24 @@
+/**
+   Encoding in utf-8
+ **/
+<meta charset="utf-8">
+     
+/**
+   Importing minified bootstrap css.
+**/
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+     /**
+        Importing bootstrap theme css.
+      **/
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+     /**
+        Importing Jquery API.
+      **/
+     <script src="jquery.min.js"></script>
+
+     /**
+        Importing javascript bootstrap.
+      **/
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
